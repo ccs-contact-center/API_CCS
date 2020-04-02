36937459faf4653c90afa37023d74eb346a27cfa
Isaac Contreras <iacontrerasg@icloud.com>
2020-04-02 17:49:50 -0600

    Update API.js

--- API.js
+++ API.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-type,Authorization");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "POST, PUT, GET, OPTIONS,PATCH"
+    "POST, PUT, GET, OPTIONS,PATCH, DELETE"
   );
   next();
 });
