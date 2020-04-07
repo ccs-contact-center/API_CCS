feb584b05465dfa99840b4951f18db464c70a5de
Isaac Contreras <iacontrerasg@icloud.com>
2020-04-07 18:25:06 -0500

    Socket.io

--- API.js
+++ API.js
@@ -1,9 +1,9 @@
 var express = require("express");
-
 var bodyParser = require("body-parser");
 var exjwt = require("express-jwt");
 var path = require("path");
 var compression = require("compression");
+const socketIo = require("socket.io");
 
 const PORT = process.env.PORT;
 //const PORT = 8082;
@@ -33,12 +33,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Instantiating the express-jwt middleware
 const jwtMW = exjwt({
-  secret: "Grhzu92E_s3cr3t"
+  secret: "Grhzu92E_s3cr3t",
 });
 
 //Para correr como standalone server
 
-const server = app.listen(PORT, function() {
+const server = app.listen(PORT, function () {
   var port = server.address().port;
   //console.clear()
   console.log("Up & Running on port " + port);
@@ -46,6 +46,27 @@ const server = app.listen(PORT, function() {
 
 //Para correr como modulo
 //exports.app = app
+const io = socketIo(server); // < Interesting!
+
+app.io = io;
+
+/*io.on("connection", function (socket) {
+  console.log(`client: ${socket.id}`);
+  socket.emit("server/random", Math.random());
+  //enviando numero aleatorio cada dos segundo al cliente
+  setInterval(() => {
+    socket.emit("server/random", Math.random());
+  }, 2000);
+  //recibiendo el numero aleatorio del cliente
+  socket.on("client/random", (num) => {
+    console.log(num);
+  });
+});*/
+
+app.get("/Socket", function (req, res) {
+  req.app.io.emit("server/random", req.query.msg);
+  res.send("OK");
+});
 
 app.use("/v1", require("./routes/v1"));
 app.use("/test", require("./routes/test"));
