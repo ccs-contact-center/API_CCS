de6d7d8f6d8a934954c4079d4581fbf71af88ea2
Isaac Contreras <iacontrerasg@icloud.com>
2020-05-01 19:55:34 -0500

    Mitrol Nuevo XML Parser

--- API.js
+++ API.js
@@ -2,11 +2,14 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exjwt = require("express-jwt");
 var path = require("path");
+var convert = require("xml-js");
+
+var fetch = require("node-fetch");
 //var compression = require("compression");
 var socketIo = require("socket.io");
 
-const PORT = process.env.PORT;
-//const PORT = 8082;
+//const PORT = process.env.PORT;
+const PORT = 8082;
 var app = express();
 
 //app.use(compression());
@@ -56,7 +59,6 @@ app.io = io;
 
 var clients = {};
 io.sockets.on("connection", (socket) => {
-
   socket.on("loginUser", (data) => {
     if (clients[data.username]) {
       //Indica que el usuario ya está conectado y no lo registra en la userlist
@@ -117,7 +119,6 @@ io.sockets.on("connection", (socket) => {
     }
     console.table(clients);
   });
-
 
   socket.on("disconnect", (data) => {
     var name = null;
@@ -190,6 +191,56 @@ app.get("/Socket/Clientes/:username", function (req, res) {
   } else {
     res.send({ logged: false });
   }
+});
+
+getMitrol = async (wsParameter) => {
+  const response = await await fetch(
+    "http://10.0.3.94/reportes/ws.asmx/MitrolWS_UserPass?Username=icontreras&Password=icontreras&wsParameter=" +
+      encodeURIComponent(wsParameter)
+  );
+  var token = await response.text();
+
+  return token;
+};
+
+app.get("/Mitrol", async (req, res) => {
+
+  var wsParameter = `
+      store=REP_RDL_EficienciaCampania05101520##
+      idPermiso=REPORTES_Campañas_Eficiencia+de+Campaña+05-10-15-20##
+      Version=17##
+      col=##ssss
+      Idioma=es##
+      SizeWeb=300##
+      SizePrint=29.7||21||cm##
+      Orientacion=H##
+      Ocultar=4||##
+      OrderBy=NombreCampania#A||##
+      UserId=5325##
+      PEXT_Query=##
+      PEXT_TipoSalidaRep=0##
+      PEXT_WS=1||##
+      RDLC_RowsByPage=-1##
+      PEXT_MaxRow=15000##
+      RDLC_FormInt=0##
+      RDLC_Culture=es-AR##
+      RDLC_strDateFormat=dd/MM/yyyy##
+      PEXT_FechaRango=-2##
+      PEXT_idEmpresa=1007||##
+      PEXT_idCamp=-1##
+      RDLC_PorInt=1##
+      RDLC_AgrupadoPor=1##
+      HIDD_fromExecuteSQL=##
+      brw=Chrome##
+      brwver=81##`;
+
+  var aber = await getMitrol(wsParameter);
+  var result = convert.xml2json(aber, { compact: true });
+  var json = JSON.parse(result);
+
+
+
+  res.send(json.DataSet["diffgr:diffgram"].NewDataSet.Table1);
 });
 
 app.use("/v1", require("./routes/v1"));
