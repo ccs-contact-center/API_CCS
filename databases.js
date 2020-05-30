2daf878301ba5a595096f52402b70da7fc4d73d0
Isaac Contreras <iacontrerasg@icloud.com>
2020-05-29 19:28:53 -0500

    Auth V2

---
+++ databases.js
@@ -0,0 +1,34 @@
+module.exports = {
+  dbCluster: {
+    user: "sa",
+    password: "Grhzu92E_db",
+    server: "10.0.0.152",
+    database: "CasasAtlas",
+    connectionTimeout: 800000,
+    requestTimeout: 800000,
+  },
+
+  db40: {
+    user: "sa",
+    password: "PPl4t3nt0?",
+    server: "10.0.0.40",
+    database: "Ventas_Bitacora",
+    connectionTimeout: 800000,
+    requestTimeout: 800000,
+  },
+  db245: {
+    user: "sa",
+    password: "Grhzu92E_db",
+    server: "10.0.0.245\\CCS",
+    database: "CCS",
+    connectionTimeout: 800000,
+    requestTimeout: 800000,
+  },
+
+  currentHost: "http://api.ccscontactcenter.com/",
+  mailAccount: "ccs.notificaciones@ccscontactcenter.com",
+  mailPass: "Pow25925",
+  mailHost: "smtp.office365.com",
+  mailPort: 587,
+  mailSecure: false,
+};
