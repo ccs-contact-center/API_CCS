08215090462c904afed4c03cd612845ec80a2e85
Isaac Contreras <iacontrerasg@icloud.com>
2020-08-06 15:19:02 -0500

    API Price Shoes

--- routes/v1/campaigns/general/general.js
+++ routes/v1/campaigns/general/general.js
@@ -1,33 +1,56 @@
-var router = require('express').Router();
+var router = require("express").Router();
 
-
-var constants = require('../../../../constants');
+var constants = require("../../../../constants");
 var utils = require("../../../../utils.js");
 
-var exjwt = require('express-jwt');
+var exjwt = require("express-jwt");
 
 const jwtMW = exjwt({
-    secret: 'Grhzu92E_s3cr3t'
+  secret: "Grhzu92E_s3cr3t",
 });
 
-
-
 //Mantenido por compatibilidad con la app
-router.get('/Application_Data',jwtMW,function(req, res) {
-    var procedure = "[CCS].[dbo].[REP_WEB_REST_Aplicacion] @CAMPANIA = " + req.query.campania + " , @TIPO = " + req.query.tipo;
-    utils.executeQuery(res, procedure);
-})
+router.get("/Application_Data", jwtMW, function (req, res) {
+  var procedure =
+    "[CCS].[dbo].[REP_WEB_REST_Aplicacion] @CAMPANIA = " +
+    req.query.campania +
+    " , @TIPO = " +
+    req.query.tipo;
+  utils.executeQuery(res, procedure);
+});
 
+router.get("/General", jwtMW, function (req, res) {
+  var procedure =
+    "[CCS].[dbo].[REP_WEB_REST_Campanias_General] @TOTALIZADO =" +
+    req.query.totalizado +
+    ",@CAMPANIA = " +
+    req.query.campania +
+    " , @TIPO = " +
+    req.query.tipo;
+  utils.executeQuery(res, procedure);
+});
 
-router.get('/General',jwtMW,function(req, res) {
-    var procedure = "[CCS].[dbo].[REP_WEB_REST_Campanias_General] @TOTALIZADO =" + req.query.totalizado + ",@CAMPANIA = " + req.query.campania + " , @TIPO = " + req.query.tipo;
-    utils.executeQuery(res, procedure);
-})
+router.get("/GeneralPS", function (req, res) {
+  var procedure =
+    "[CCS].[dbo].[REP_WEB_REST_Campanias_General_PS] @TOTALIZADO =" +
+    req.query.totalizado +
+    ",@CAMPANIA = " +
+    req.query.campania +
+    " , @TIPO = " +
+    req.query.tipo +
+    ", @FECHA_1= '" +
+    req.query.fecha_ini +
+    "', @FECHA_2 = '" +
+    req.query.fecha_fin +
+    "'";
+  utils.executeQuery(res, procedure);
+});
 
-
-router.get('/Objetivos',jwtMW,function(req, res) {
-    var query = "SELECT DISTINCT id_campania ,campania ,SLA,ABA,AHT,TT ,OCCY,QA FROM CCS.dbo.Campanias WHERE id_campania = " + req.query.campania;
-    utils.executeQuery(res, query);
-})
+router.get("/Objetivos", jwtMW, function (req, res) {
+  var query =
+    "SELECT DISTINCT id_campania ,campania ,SLA,ABA,AHT,TT ,OCCY,QA FROM CCS.dbo.Campanias WHERE id_campania = " +
+    req.query.campania;
+  utils.executeQuery(res, query);
+});
 
 module.exports = router;
